3933dd306d123f371624c225f93894e748a2e723
hari-hk <hariharakumar5196@gmail.com>
2023-06-13 22:09:06 +0530

    update style

--- src/pages/home/home.page.jsx
+++ src/pages/home/home.page.jsx
@@ -29,22 +29,46 @@ const HomePage = () => {
 
   return (
     <div className='cover-page'>
-      <img
+      {/* <img
         className='cover-image'
         src='/assets/images/landing.png'
         alt=''
         srcset=''
-      />
+      /> */}
 
-      <div className='list'>
-        <div className='item'>
-          <div className='title'>Header</div>
-          <Masker
+      <div className='toolbar'>
+        <div className='title'>SAYANAN</div>
+        <div className='sub-title'>Digital Artist</div>
+      </div>
+      <div className='content'>
+        <div className='summary'>
+          <div className='objective'>
+            <h1>Hello !!</h1>
+            <p>
+              I'm  <b>Ananda Sayanan</b> a talented 3D artist with a passion for creating stunning
+              visual experiences. Explore my portfolio and discover the world of
+              3D art.
+            </p>
+          </div>
+       <div className="object">
+       <Masker
             size={500}
             background={'/assets/images/1-fore.png'}
             foreground={'/assets/images/1-back.png'}
           />
+       </div>
         </div>
+
+        {/* <div className='list'>
+          <div className='item'>
+            <div className='title'>Props Model</div>
+            <Masker
+              size={500}
+              background={'/assets/images/1-fore.png'}
+              foreground={'/assets/images/1-back.png'}
+            />
+          </div>
+        </div> */}
       </div>
     </div>
   );
